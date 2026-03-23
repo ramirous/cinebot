d5f2715dd6ffebe8756be12a83fb97dcb6d694be
root <ramiroelizondog@gmail.com>
2026-03-22 19:48:43 -0600

    Limpiar repo: quitar venv, config y archivos innecesarios

--- config.py
+++
@@ -1,9 +0,0 @@
-TELEGRAM_TOKEN   = "8506444894:AAGFRHsrWCihsSWy5gtJdWH_zVlh1qG3mQ8"
-ALLOWED_USER_ID  = 1557717415
-
-QB_HOST          = "localhost"
-QB_PORT          = 8080
-QB_USER          = "admin"
-QB_PASS          = "lourdesrr"
-
-#DOWNLOAD_PATH    = "/mnt/DatosF/PelículasF"
